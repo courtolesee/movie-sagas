6e90cb0324b6fe37b54baeb7203475fa7c279635
Courtney Olesee <court.olson1@gmail.com>
2019-12-23 17:30:19 -0600

    more css on home page movie cards

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import MovieCard from '../MovieCard/MovieCard'
+import MovieCard from '../MovieCard/MovieCard';
 
 // home page - maps list of movies
 class Home extends Component {
@@ -10,9 +10,9 @@ class Home extends Component {
         <section>
             <p className="instructions">Click on a movie poster for more details about that movie!</p>
             <div className="imageGrid">
-            {this.props.movies.map( (movie,i)=> {     
-            return <MovieCard key={i} movie={movie}/>
-            })}
+              {this.props.movies.map( (movie,i)=> {     
+              return <MovieCard key={i} movie={movie}/>
+              })}
             </div>
         </section>
       )
