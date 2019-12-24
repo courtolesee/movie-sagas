0b40422a062361bb6cea3773dc009ea5bbc9a624
Courtney Olesee <court.olson1@gmail.com>
2019-12-23 19:58:18 -0600

    css on details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -19,10 +19,10 @@ class Details extends Component {
         <h1>Details</h1>
         <button onClick={this.goBackHome}>Back to Movie List</button>
         <div>
-            <img src={this.props.movie.poster} alt={this.props.movie.title} onClick={this.goToDetails}/>
-            <p>{this.props.movie.title}</p>
-            <p>{this.props.movie.genres.join(', ')}</p>
-            <p>{this.props.movie.description}</p>
+            <img className="poster" src={this.props.movie.poster} alt={this.props.movie.title} onClick={this.goToDetails}/>
+            <p className="title">{this.props.movie.title}</p>
+            <p className="genresDetails">{this.props.movie.genres.join(', ')}</p>
+            <p className="descriptionDetails">{this.props.movie.description}</p>
             <button onClick={this.goToEdit}>Edit</button>
         </div>
       </div>
